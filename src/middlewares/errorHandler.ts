9bbee75cc27a949afa7e192360e2c0cc88c7434b
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-07-11 23:37:16 +0800

    Finished shippable featureset

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -4,34 +4,35 @@ import { Prisma } from '@prisma/client';
 import { AuthError } from '../modules/auth/auth.errors';
 
 export const prismaErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
-  if (error instanceof Prisma.PrismaClientKnownRequestError) {
-    if (error.code.match('P2002')) {
-      const message = `The ${error?.meta?.target} should be unique. `;
-      clientErrResponseHandler(res, { message }, 400);
-      return;
-    }
-    if (error.code.match('P2025')) {
-      clientErrResponseHandler(
-        res,
-        { code: error.code, message: 'You were trying to edit a record that does not exist.' },
-        422
-      );
-      return;
-    }
-    serverErrResponseHandler(res, {
-      code: error.code,
-      message:
-        'Congrats! You found a new PrismaClientKnownRequestError. Tell the admins about it so they can catch it. For now it is an error 500'
-    });
-    return;
-  }
-  if (error instanceof Prisma.PrismaClientValidationError) {
-    clientErrResponseHandler(res, {
-      code: "Prisma Client Validation Error",
-      message: "Invalid invocation. You may be sending an invalid input to one of API fields."
-    });
-    return;
-  }
+  // if (error instanceof Prisma.PrismaClientKnownRequestError) {
+  //   if (error.code.match('P2002')) {
+  //     const message = `The ${error?.meta?.target} should be unique. `;
+  //     clientErrResponseHandler(res, { message }, 400);
+  //     return;
+  //   }
+  //   if (error.code.match('P2025')) {
+  //     clientErrResponseHandler(
+  //       res,
+  //       { code: error.code, message: 'You were trying to edit a record that does not exist.' },
+  //       422
+  //     );
+  //     return;
+  //   }
+  //   serverErrResponseHandler(res, {
+  //     code: error.code,
+  //     message:
+  //       'Congrats! You found a new PrismaClientKnownRequestError. Tell the admins about it so they can catch it. For now it is an error 500'
+  //   });
+  //   return;
+  // }
+  // if (error instanceof Prisma.PrismaClientValidationError) {
+  //   clientErrResponseHandler(res, {
+  //     code: "Prisma Client Validation Error",
+  //     message: "Invalid invocation. You may be sending an invalid input to one of API fields.",
+  //     error
+  //   });
+  //   return;
+  // }
   next(error);
   return;
 };
@@ -41,6 +42,7 @@ export const genericErrorHandler: ErrorRequestHandler = (error, req, res, next) 
     clientErrResponseHandler(res, error.message, 403);
     return;
   }
-  serverErrResponseHandler(res, error.message);
+  // serverErrResponseHandler(res, error.message);
+  throw error
   return;
 };
